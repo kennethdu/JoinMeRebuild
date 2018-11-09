8546e07568c637e30362d11a39ffc4c89a5c20b2
Kenneth Du <kennethdu3@gmail.com>
2018-11-09 14:58:28 -0800

    add access to apikey

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyA1DZzZgnm9-LNINaQ_Q4046Y1mtSJjeBw",
+  authDomain: "join-me-rebuild.firebaseapp.com",
+  databaseURL: "https://join-me-rebuild.firebaseio.com",
+  // projectId: "join-me-rebuild",
+  storageBucket: "join-me-rebuild.appspot.com",
+  messagingSenderId: "478894879279"
+
+};
